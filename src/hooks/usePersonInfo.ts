78656128fc81c8f519ac2f4406a8b123ad91feaa
Zhenxue Wu <wu565626236@gmail.com>
2024-08-04 17:01:40 +0800

    update

--- src/hooks/usePersonInfo.ts
+++ src/hooks/usePersonInfo.ts
@@ -9,13 +9,12 @@ const usePersonInfo = () => {
   const startWorkTime =  dayjs("20130301", "YYYYMMDD");
   const now = dayjs();
   const age = dayjs.duration(now.diff(birthday)).years();
-
   const yearsOfWorking = dayjs.duration(now.diff(startWorkTime)).years();
 
   const personInfo = ref({
     name: "吴振雪",
     gender: "男",
-    phone: "15010057138",
+    phone: "150-1005-7138",
     email: "15010057138@163.com",
     birthday: "19891227",
     age,
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.scss'
 import App from './App.vue'
+import 'normalize.css/normalize.css'
+import './style.scss'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.mount('#app')
